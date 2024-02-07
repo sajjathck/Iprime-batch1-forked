afc539a788a14e3e0e31d6786a67202bcb88587f
ckSajjad <51065550+sajjathck@users.noreply.github.com>
2024-02-07 12:06:21 +0530

    ss

--- WEB/React/HandsOn/SMSAPI/Program.cs
+++ WEB/React/HandsOn/SMSAPI/Program.cs
@@ -14,11 +14,21 @@ namespace SMSAPI
             builder.Services.AddTransient<StudentRepository>();
             builder.Services.AddDbContext<MyContext>();
             builder.Services.AddControllers();
+            builder.Services.AddCors(c =>       
+            {
+                c.AddPolicy("AllowOrigin", options =>
+                {
+                    options.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -28,7 +38,7 @@ namespace SMSAPI
             }
 
             app.UseAuthorization();
-
+            app.UseCors("AllowOrigin");
 
             app.MapControllers();
 
